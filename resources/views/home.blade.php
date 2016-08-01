800600d8691aa9128a6cd3986d8c6738b012432f
Liam Demafelix <liamdemafelix.n@gmail.com>
2016-08-01 14:06:03 +0800

    Fix decode URL

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
             @else
                 @foreach ($questions as $question)
                     <tr>
-                        <td>{{ $question->question }}</td>
+                        <td>{{ urldecode($question->question) }}</td>
                         <td class="text-center">{{ $question->asked_by }}</td>
                         <td class="text-center">{{ \Carbon\Carbon::createFromFormat("Y-m-d G:i:s", $question->created_at)->format("F j, Y g:i A") }}</td>
                         <td class="text-center"><button type="button" onclick="sendAnswer({{ $question->id }}, '{{ $question->question }}')" class="btn btn-primary">Answer</button></td>
